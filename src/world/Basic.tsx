2006c553f1e7e28e3355819b8f9ee3a1498001af
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-02 17:09:12 +0900

    feat : add click handler

--- src/world/Basic.tsx
+++ src/world/Basic.tsx
@@ -1,7 +1,7 @@
 import { Vector3 } from '@babylonjs/core/Maths/math.vector'
 import { Scene, Engine } from 'react-babylonjs'
 import React, { FC } from 'react'
-
+import MyBox from './model/Box';
 const Basic: FC = () => {
 	return (
 		<div style={{ flex: 1, display: 'flex' }}>
@@ -25,12 +25,7 @@ const Basic: FC = () => {
 						direction={new Vector3(0, 1, 0)}
 					/>
 					<ground name="ground" width={6} height={6} />
-					<box
-						name="box"
-						size={2}
-						position={new Vector3(0, 1, 0)}
-						rotation={Vector3.Zero()}
-					/>
+					<MyBox name="hello" position={new Vector3(0, 3, 0)}></MyBox>
 				</Scene>
 			</Engine>
 		</div>
--- src/world/model/Box.tsx
+++ src/world/model/Box.tsx
@@ -11,49 +11,25 @@ import { Vector3, Color3 } from '@babylonjs/core'
 const DefaultScale = new Vector3(1, 1, 1)
 const BiggerScale = new Vector3(1.25, 1.25, 1.25)
 
-const SpinningBox = (props) => {
+const MyBox = (props) => {
 	// access Babylon scene objects with same React hook as regular DOM elements
-	const boxRef = useRef(null)
+	//const boxRef = useRef(null)
+	const boxRef = useRef(null);
 
-	const [clicked, setClicked] = useState(false)
-	useClick(() =>
-		console.log("why click me ...?");
-
-
-
-	setClicked((clicked) => !clicked), boxRef)
-
-	const [hovered, setHovered] = useState(false)
-	useHover(
-		() => setHovered(true),
-		() => setHovered(false),
-		boxRef
-	)
-
-	// This will rotate the box on every Babylon frame.
-	const rpm = 5
-	useBeforeRender((scene) => {
-		if (boxRef.current) {
-			// Delta time smoothes the animation.
-			var deltaTimeInMillis = scene.getEngine().getDeltaTime()
-			boxRef.current.rotation.y +=
-				(rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000)
-		}
-	})
+	useClick(() => {
+		console.log("why you click me?") // Always logs 0. How to access the current count here?
+	}, boxRef)
 
 	return (
-		<box
-			name={props.name}
-			ref={boxRef}
-			size={2}
-			position={props.position}
-			scaling={clicked ? BiggerScale : DefaultScale}
-		>
+		<box ref={boxRef} name="counter" size={10} >
 			<standardMaterial
 				name={`${props.name}-mat`}
-				diffuseColor={hovered ? props.hoveredColor : props.color}
+				diffuseColor={props.color}
 				specularColor={Color3.Black()}
 			/>
-		</box>
+		</box >
 	)
 }
+
+
+export default MyBox;
